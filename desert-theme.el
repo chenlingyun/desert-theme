31c6ee492857ec6fa9019e8f24fc07d22c5db10d
rexim <reximkut@gmail.com>
2013-06-15 21:41:40 +0700

    Resolved #1

--- desert-theme.el
+++ desert-theme.el
@@ -60,31 +60,38 @@
    `(font-lock-string-face ((t (:foreground ,desert-pink))))
    `(font-lock-type-face ((t (:foreground ,desert-green :bold t))))
    `(font-lock-variable-name-face ((t (:foreground ,desert-fg))))
-   `(font-lock-warning-face ((t (:foreground ,desert-warning-fg :bold t))))
+   `(font-lock-warning-face ((t (:foreground ,desert-warning-fg :bold t :inherit nil))))
    `(font-lock-function-name-face ((t (:foreground ,desert-green :bold t))))
    `(font-lock-constant-face ((t (:foreground ,desert-pink))))
 
    `(minibuffer-prompt ((t (:foreground ,desert-khaki :bold t))))
-   `(Buffer-menu-buffer ((t (:inherit minibuffer-prompt))))
+   `(Buffer-menu-buffer ((t (:foreground ,desert-khaki))))
    `(header-line ((t (:background ,desert-bg-1 :box (:color ,desert-bg :line-width 2)))))
-   `(mode-line ((t (:inherit header-line :foreground ,desert-bg+2))))
+   `(mode-line ((t (:inherit header-line :foreground ,desert-bg+2 :background ,desert-bg-1))))
    `(mode-line-inactive ((t (:inherit mode-line))))
-   `(mode-line-buffer-id ((t (:inherit mode-line :foreground ,desert-warning-fg :bold t))))
+   `(mode-line-buffer-id ((t (:foreground ,desert-warning-fg :bold t))))
 
-   `(linum ((t (:foreground ,desert-yellow :background ,desert-bg))))
+   `(linum ((t (:foreground ,desert-yellow :background ,desert-bg :inherit nil))))
    `(highlight ((t (:foreground ,desert-khaki :background ,desert-olive))))
    `(region ((t (:background nil :inherit highlight))))
-   `(show-paren-mismatch ((t (:inherit font-lock-warning-face :bold t))))
-   `(show-paren-match ((t (:foreground ,desert-green :bold t))))
-   `(trailing-whitespace ((t (:inherit font-lock-warning-face))))
+   `(show-paren-mismatch ((t (:foreground ,desert-red :background ,desert-bg :bold t))))
+   `(show-paren-match ((t (:foreground ,desert-fg :background "darkcyan" :bold t))))
+   `(trailing-whitespace ((t (:background nil :inherit font-lock-warning-face))))
    `(match ((t (:weight bold))))
 
+   ;; link
    `(link ((t (:background ,desert-bg :foreground ,desert-red :bold t :underline nil))))
    `(link-visited ((t (:inherit link :bold nil))))
 
+   ;; isearch
    `(isearch ((t (:foreground ,desert-khaki :background ,desert-olive))))
    `(isearch-lazy-light ((t (:background ,desert-bg :foreground ,desert-fg :bold t))))
 
+   ;; compilation
+   `(compilation-info ((t (:foreground ,desert-green :bold t :inherit nil))))
+   `(compilation-warning ((t (:foreground ,desert-khaki :bold t :inherit nil))))
+
+   ;; jabber.el
    `(jabber-roster-user-chatty ((t (:inherit font-lock-type-face :bold t))))
    `(jabber-roster-user-online ((t (:inherit font-lock-keyword-face :bold t))))
    `(jabber-roster-user-offline ((t (:foreground ,desert-bg+1 :background ,desert-bg))))
@@ -105,14 +112,17 @@
    `(jabber-activity-face ((t (:inherit jabber-chat-prompt-foreign))))
    `(jabber-activity-personal-face ((t (:inherit jabber-chat-prompt-local :bold t))))
 
+   ;; ido
    `(ido-first-match ((t (:foreground ,desert-green :bold t))))
    `(ido-only-match ((t (:foreground ,desert-green :bold t))))
-   `(ido-subdir ((t (:foreground nil :inherit minibuffer-prompt))))
+   `(ido-subdir ((t (:foreground ,desert-khaki :bold t))))
 
+   ;; auto-complete
    `(ac-candidate-face ((t (:background ,desert-bg-1 :foreground ,desert-fg))))
    `(ac-selection-face ((t (:inherit highlight))))
    `(ac-completion-face ((t (:inherit ac-selection-face))))
 
+   ;; elscreen
    `(elscreen-tab-background-face ((t (:background ,desert-bg-1))))
    `(elscreen-tab-other-screen-face
      ((t (:background ,desert-bg-1 :foreground ,desert-bg+2))))
